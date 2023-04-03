e85f3828e6186ea4ab2134df3752fe64087dd2b4
arturocfr <arturofabian504@gmail.com>
2023-04-03 16:17:13 +0200

    1

---
+++ m1/s2/g1/compito1/js/script.js
@@ -0,0 +1,3 @@
+document.getElementById("saluto").innerHTML = '<b>Hello World!</b>';
+document.getElementById("nome").innerHTML = '<em>Giovanna</em>';
+document.getElementById("giorno").innerHTML = 'venerdì';
