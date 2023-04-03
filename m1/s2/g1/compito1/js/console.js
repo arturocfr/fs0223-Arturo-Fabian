b24324526ff7aadbf0eb8874fa5f387f9b1465ca
arturocfr <arturofabian504@gmail.com>
2023-04-03 16:17:02 +0200

    1

---
+++ m1/s2/g1/compito1/js/console.js
@@ -0,0 +1,41 @@
+function saluto(){
+    console.log('Hello World!');
+    console.log('Hello World!');
+    console.log('Hello World!');
+    console.log('Hello World!');
+}
+saluto();
+saluto();
+
+
+function animali(){
+    console.log('zebra')
+    console.log('leone')
+    console.log('giraffa')
+    console.log('iena')
+}
+animali();
+saluto();
+
+
+
+var risposta = true;
+if(risposta){
+    console.log("va bene");
+
+}else{
+    console.log("non va bene");
+}
+
+
+
+var o = 5;
+if(o >= 5){
+    console.log('alto');
+}
+else if(o == 5){
+    console.log('ugualeoooooo');
+
+}else if (o < 5){
+    console.log('basso');
+}
