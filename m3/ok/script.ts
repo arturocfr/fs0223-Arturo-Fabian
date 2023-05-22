df506062d067eb9dc8aeafa4078a2d95a474d5cb
arturocfr <arturofabian504@gmail.com>
2023-05-22 12:21:25 +0200

    capito gitignore

---
+++ m3/ok/script.ts
@@ -0,0 +1,14 @@
+console.log('Hello world!');
+
+let nome:string = 'Mario';
+let anni:number = 30;
+let boolean:boolean = false;
+
+let array:any[] = [20 ,'testo',true];
+let arrayString:string[] = ['testo'];
+let arrayNumber:number[] = [20,30,40];
+
+nome ='marco';
+
+let vuota:undefined|number;
+vuota = 0;
