18cbd1fb952e3c1d6c8c3f32b4f46e73e0f70614
arturocfr <arturofabian504@gmail.com>
2023-05-25 17:05:34 +0200

    bho

--- m3/s1/g4/starter/modules/script.js
+++ m3/s1/g4/starter/modules/script.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createEl = void 0;
 function createEl(_tagName, _classes, _text) {
-    var el = document.createElement(_tagName);
-    _classes ? _classes.forEach(function (cl) { return el.classList.add(cl); }) : 0;
+    let el = document.createElement(_tagName);
+    _classes ? _classes.forEach((cl) => el.classList.add(cl)) : 0;
     _text ? (el.innerText = _text) : 0;
     return el;
 }
