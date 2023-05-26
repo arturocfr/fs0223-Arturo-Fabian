a370d426a349dc16c0e7acdde3270e712c3b47f0
arturocfr <arturofabian504@gmail.com>
2023-05-26 16:46:35 +0200

    va bene

--- m3/s1/g5/assets/script.js
+++ m3/s1/g5/assets/script.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var function_js_1 = require("./module/module.js");
-var Smartphone = /** @class */ (function () {
-    function Smartphone(_carica, _elementsHTML) {
+const module_js_1 = require("./module/module.js");
+class Smartphone {
+    constructor(_carica, _elementsHTML) {
         this.numeroChiamate = 0;
         this.costoMinuto = 0.2;
         this.registroChiamate = [];
@@ -12,33 +12,25 @@ var Smartphone = /** @class */ (function () {
         this.updateHTML();
         this.setupBtns();
     }
-    Smartphone.prototype.ricarica = function (euro) {
+    ricarica(euro) {
         var _a;
         this.carica += euro;
         this.updateHTML();
         (_a = this.elementsHTML.errore) === null || _a === void 0 ? void 0 : _a.classList.add("opacity-0");
-    };
-    Object.defineProperty(Smartphone.prototype, "numero404", {
-        get: function () {
-            return "Il tuo credito residuo \u00E8 di: ".concat(this.carica.toFixed(2), "\u20AC");
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Smartphone.prototype, "getNumeroChiamate", {
-        get: function () {
-            return this.registroChiamate.length;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Smartphone.prototype.chiamata = function (min) {
+    }
+    get numero404() {
+        return `Il tuo credito residuo è di: ${this.carica.toFixed(2)}€`;
+    }
+    get getNumeroChiamate() {
+        return this.registroChiamate.length;
+    }
+    chiamata(min) {
         var _a, _b;
-        var calcolaCosto = this.carica - this.costoMinuto * min;
+        let calcolaCosto = this.carica - this.costoMinuto * min;
         if (calcolaCosto > 0) {
             (_a = this.elementsHTML.errore) === null || _a === void 0 ? void 0 : _a.classList.add("opacity-0");
-            var now = "".concat(new Date().getDay() + 1, "/").concat(new Date().getMonth() + 1, "/").concat(new Date().getFullYear());
-            var callDetails = {
+            let now = `${new Date().getDay() + 1}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`;
+            let callDetails = {
                 id: this.callCounter,
                 durata: min,
                 date: now,
@@ -51,52 +43,47 @@ var Smartphone = /** @class */ (function () {
         else {
             (_b = this.elementsHTML.errore) === null || _b === void 0 ? void 0 : _b.classList.remove("opacity-0");
         }
-    };
-    Smartphone.prototype.azzeraChiamate = function () {
+    }
+    azzeraChiamate() {
         var _a;
         this.registroChiamate = [];
         this.callCounter = 0;
         this.updateHTML();
         (_a = this.elementsHTML.errore) === null || _a === void 0 ? void 0 : _a.classList.add("opacity-0");
         this.elementsHTML.callsList.innerHTML = "";
-    };
-    Object.defineProperty(Smartphone.prototype, "mostraRegistroChiamate", {
-        get: function () {
-            return this.registroChiamate;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Smartphone.prototype.filtraChiamatePerDataOra = function (str) {
+    }
+    get mostraRegistroChiamate() {
+        return this.registroChiamate;
+    }
+    filtraChiamatePerDataOra(str) {
         var _a;
         (_a = this.elementsHTML.errore) === null || _a === void 0 ? void 0 : _a.classList.add("opacity-0");
-        return this.registroChiamate.filter(function (el) {
+        return this.registroChiamate.filter((el) => {
             el.date === str;
         });
-    };
-    Smartphone.prototype.updateHTML = function () {
+    }
+    updateHTML() {
         this.elementsHTML.credito.innerText = this.numero404;
-        this.elementsHTML.numCalls.innerText = "".concat(this.registroChiamate.length);
-    };
-    Smartphone.prototype.setupBtns = function () {
-        var _this = this;
+        this.elementsHTML.numCalls.innerText = `${this.registroChiamate.length}`;
+    }
+    setupBtns() {
         var _a, _b, _c, _d;
-        (_a = this.elementsHTML.btnRicarica) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
-            _this.ricarica(5);
+        (_a = this.elementsHTML.btnRicarica) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+            this.ricarica(5);
         });
-        (_b = this.elementsHTML.btnAzzeraChiamate) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function () {
-            _this.azzeraChiamate();
+        (_b = this.elementsHTML.btnAzzeraChiamate) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => {
+            this.azzeraChiamate();
         });
-        (_c = this.elementsHTML.btnEffettuaChiamata) === null || _c === void 0 ? void 0 : _c.addEventListener("click", function () {
-            _this.chiamata(Number(_this.elementsHTML.inputMin.value));
-            _this.elementsHTML.inputMin.value = "";
+        (_c = this.elementsHTML.btnEffettuaChiamata) === null || _c === void 0 ? void 0 : _c.addEventListener("click", () => {
+            this.chiamata(Number(this.elementsHTML.inputMin.value));
+            this.elementsHTML.inputMin.value = "";
         });
-        (_d = this.elementsHTML.btnMostraRegistro) === null || _d === void 0 ? void 0 : _d.addEventListener("click", function () {
-            _this.elementsHTML.callsList.innerHTML = "";
-            _this.mostraRegistroChiamate.forEach(function (el) {
+        (_d = this.elementsHTML.btnMostraRegistro) === null || _d === void 0 ? void 0 : _d.addEventListener("click", () => {
+            this.elementsHTML.callsList.innerHTML = "";
+            this.mostraRegistroChiamate.forEach((el) => {
                 var _a;
                 //
-                var col = (0, function_js_1.createEl)("div", [
+                let col = (0, module_js_1.createEl)("div", [
                     "col",
                     "text-center",
                     "pb-2",
@@ -105,49 +92,48 @@ var Smartphone = /** @class */ (function () {
                     "border-1",
                     "border-black",
                 ]);
-                var primo_p = (0, function_js_1.createEl)("p", ["m-0"], "ID: ".concat(el.id));
-                var secondo_p = (0, function_js_1.createEl)("p", ["m-0", "mt-1"], "Durata: ".concat(el.durata));
-                var terzo_p = (0, function_js_1.createEl)("p", ["m-0"], "Data: ".concat(el.date));
+                let primo_p = (0, module_js_1.createEl)("p", ["m-0"], `ID: ${el.id}`);
+                let secondo_p = (0, module_js_1.createEl)("p", ["m-0", "mt-1"], `Durata: ${el.durata}`);
+                let terzo_p = (0, module_js_1.createEl)("p", ["m-0"], `Data: ${el.date}`);
                 col.append(primo_p, secondo_p, terzo_p);
-                (_a = _this.elementsHTML.callsList) === null || _a === void 0 ? void 0 : _a.append(col);
+                (_a = this.elementsHTML.callsList) === null || _a === void 0 ? void 0 : _a.append(col);
             });
         });
-    };
-    return Smartphone;
-}());
+    }
+}
 window.onload = function () {
-    var els_1 = {
-        credito: (0, function_js_1.byid)("credito-one"),
-        numCalls: (0, function_js_1.byid)("num-chiamate-one"),
-        callsList: (0, function_js_1.byid)("dettagli-chiamate-one"),
-        btnRicarica: (0, function_js_1.byid)("ric-one"),
-        inputMin: (0, function_js_1.byid)("num-min-one"),
-        btnEffettuaChiamata: (0, function_js_1.byid)("call-one"),
-        btnMostraRegistro: (0, function_js_1.byid)("reg-one"),
-        btnAzzeraChiamate: (0, function_js_1.byid)("reset-one"),
-        errore: (0, function_js_1.byid)("err-one"),
+    let els_1 = {
+        credito: (0, module_js_1.byid)("credito-one"),
+        numCalls: (0, module_js_1.byid)("num-chiamate-one"),
+        callsList: (0, module_js_1.byid)("dettagli-chiamate-one"),
+        btnRicarica: (0, module_js_1.byid)("ric-one"),
+        inputMin: (0, module_js_1.byid)("num-min-one"),
+        btnEffettuaChiamata: (0, module_js_1.byid)("call-one"),
+        btnMostraRegistro: (0, module_js_1.byid)("reg-one"),
+        btnAzzeraChiamate: (0, module_js_1.byid)("reset-one"),
+        errore: (0, module_js_1.byid)("err-one"),
     };
-    var els_2 = {
-        credito: (0, function_js_1.byid)("credito-two"),
-        numCalls: (0, function_js_1.byid)("num-chiamate-two"),
-        callsList: (0, function_js_1.byid)("dettagli-chiamate-two"),
-        btnRicarica: (0, function_js_1.byid)("ric-two"),
-        inputMin: (0, function_js_1.byid)("num-min-two"),
-        btnEffettuaChiamata: (0, function_js_1.byid)("call-two"),
-        btnMostraRegistro: (0, function_js_1.byid)("reg-two"),
-        btnAzzeraChiamate: (0, function_js_1.byid)("reset-two"),
-        errore: (0, function_js_1.byid)("err-one"),
+    let els_2 = {
+        credito: (0, module_js_1.byid)("credito-two"),
+        numCalls: (0, module_js_1.byid)("num-chiamate-two"),
+        callsList: (0, module_js_1.byid)("dettagli-chiamate-two"),
+        btnRicarica: (0, module_js_1.byid)("ric-two"),
+        inputMin: (0, module_js_1.byid)("num-min-two"),
+        btnEffettuaChiamata: (0, module_js_1.byid)("call-two"),
+        btnMostraRegistro: (0, module_js_1.byid)("reg-two"),
+        btnAzzeraChiamate: (0, module_js_1.byid)("reset-two"),
+        errore: (0, module_js_1.byid)("err-one"),
     };
-    var els_3 = {
-        credito: (0, function_js_1.byid)("credito-three"),
-        numCalls: (0, function_js_1.byid)("num-chiamate-three"),
-        callsList: (0, function_js_1.byid)("dettagli-chiamate-three"),
-        btnRicarica: (0, function_js_1.byid)("ric-three"),
-        inputMin: (0, function_js_1.byid)("num-min-three"),
-        btnEffettuaChiamata: (0, function_js_1.byid)("call-three"),
-        btnMostraRegistro: (0, function_js_1.byid)("reg-three"),
-        btnAzzeraChiamate: (0, function_js_1.byid)("reset-three"),
-        errore: (0, function_js_1.byid)("err-three"),
+    let els_3 = {
+        credito: (0, module_js_1.byid)("credito-three"),
+        numCalls: (0, module_js_1.byid)("num-chiamate-three"),
+        callsList: (0, module_js_1.byid)("dettagli-chiamate-three"),
+        btnRicarica: (0, module_js_1.byid)("ric-three"),
+        inputMin: (0, module_js_1.byid)("num-min-three"),
+        btnEffettuaChiamata: (0, module_js_1.byid)("call-three"),
+        btnMostraRegistro: (0, module_js_1.byid)("reg-three"),
+        btnAzzeraChiamate: (0, module_js_1.byid)("reset-three"),
+        errore: (0, module_js_1.byid)("err-three"),
     };
     new Smartphone(5, els_1);
     new Smartphone(15, els_2);
