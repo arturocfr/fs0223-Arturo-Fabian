a6ff69d06cea90301b4d3aeee9293e6c63ed35b7
arturocfr <arturofabian504@gmail.com>
2023-05-26 16:47:39 +0200

    va bene

--- m3/s1/g5/assets/module/module.js
+++ m3/s1/g5/assets/module/module.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.byid = exports.createEl = void 0;
 function createEl(name, classes, text, id) {
-    let el = document.createElement(name);
-    classes ? classes.forEach((cl) => el.classList.add(cl)) : 0;
+    var el = document.createElement(name);
+    classes ? classes.forEach(function (cl) { return el.classList.add(cl); }) : 0;
     text ? (el.textContent = text) : 0;
     id ? (el.id = id) : 0;
     return el;
